893d0948dc76cae46f4e44e938e7916e464f2295
joomscan <806148844@qq.com>
2015-10-09 21:21:30 +0800

    del dash

--- dash/server/modules/shell_files/bandwidth.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/bin/cat /proc/net/dev \
-| awk 'BEGIN {print "["} NR>2 {print "{ \"网卡\": \"" $1 "\"," \
-					" \"已发送\": " $2 "," \
-					" \"已接收\": " $10 " }," } END {print "]"}' \
-| /bin/sed 'N;$s/,\n/\n/;P;D'
